8ed579395702ab92fcdf931ebcfef2167739aa94
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2018-10-05 13:45:38 +0200

    test for CAM-9405

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,14 +12,32 @@
  */
 package org.camunda.bpm.unittest;
 
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
+import org.camunda.spin.DataFormats;
+import org.camunda.spin.impl.json.jackson.format.JacksonJsonDataFormat;
+import org.camunda.spin.json.SpinJsonNode;
+import org.camunda.spin.spi.DataFormat;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.type.CollectionLikeType;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**
  * @author Daniel Meyer
@@ -32,20 +50,27 @@ public class SimpleTestCase {
 
   @Test
   @Deployment(resources = {"testProcess.bpmn"})
-  public void shouldExecuteProcess() {
-    // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
+  public void shouldExecuteProcess() throws JsonParseException, JsonMappingException, IOException {
+    List<ListElement> list = new ArrayList<>();
+    ListElement element1 = new ListElement();
+    ListElement element2 = new AnotherListElement();
+    list.add(element1);
+    list.add(element2);
+    ObjectValue variableValue = Variables.objectValue(list)
+      .serializationDataFormat(Variables.SerializationDataFormats.JSON.getName())
+      .create();
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    // Given we create a new process instance
+    ProcessInstance processInstance = runtimeService()
+        .startProcessInstanceByKey("testProcess", Variables.createVariables().putValue("var", variableValue));
+
+    final ObjectValue serializedValue = rule.getRuntimeService().getVariableTyped(processInstance.getId(), "var", false);
+    System.out.println(serializedValue.getValueSerialized());
+    final List<ListElement> deserializedValue = (List<ListElement>) rule.getRuntimeService().getVariable(processInstance.getId(), "var");
+
+    assertThat(deserializedValue).hasSize(2);
+    assertThat(deserializedValue.get(0)).isInstanceOf(ListElement.class);
+    assertThat(deserializedValue.get(1)).isInstanceOf(AnotherListElement.class);
   }
 
 }
