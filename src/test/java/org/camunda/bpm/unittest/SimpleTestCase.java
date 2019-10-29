28199cce2be3a4bf0a0659fdb9915b73f359f9d1
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2019-10-29 17:30:10 +0100

    exemplary test case for call activities in heterogeneous cluster

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -17,43 +17,99 @@
 package org.camunda.bpm.unittest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processInstanceQuery;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
+import static org.assertj.core.api.Assertions.entry;
 
+import java.time.Duration;
+import java.util.Map;
+
+import org.awaitility.Awaitility;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.engine.test.ProcessEngineRule;
-import org.junit.Rule;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * @author Daniel Meyer
- * @author Martin Schimak
- */
 public class SimpleTestCase {
 
-  @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  private static final String H2_URL = "jdbc:h2:mem:camunda";
+
+  public static final String CALLING_IN_VARIABLE = "callingInput";
+  public static final String CALLING_OUT_VARIABLE = "callingOutput";
+  public static final String CALLED_IN_VARIABLE = "calledInput";
+  public static final String CALLED_OUT_VARIABLE = "calledOutput";
+
+  public static final String ENGINE_USED_FOR_IN_MAPPING = "engineUsedForInMapping";
+  public static final String ENGINE_USED_FOR_OUT_MAPPING = "engineUsedForOutMapping";
+
+  public static final String INPUT = "foo";
+  public static final String RESULT = "bar";
+
+  private ProcessEngine callingEngine;
+  private ProcessEngine calledEngine;
+
+  @Before
+  public void createProcessEngines() {
+    callingEngine = buildEngine("callingEngine");
+    calledEngine = buildEngine("calledEngine");
+
+    Awaitility.setDefaultPollInterval(Duration.ofMillis(100));
+  }
+
+  private ProcessEngine buildEngine(String name) {
+    return ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
+      .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+      .setJdbcUrl(H2_URL)
+      .setJobExecutorDeploymentAware(true)
+      .setJobExecutorActivate(true)
+      .setProcessEngineName(name)
+      .buildProcessEngine();
+  }
 
   @Test
-  @Deployment(resources = {"testProcess.bpmn"})
   public void shouldExecuteProcess() {
-    // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
+    // given
+    callingEngine.getRepositoryService().createDeployment()
+      .addClasspathResource("callingProcess.bpmn")
+      .deploy();
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    calledEngine.getRepositoryService().createDeployment()
+      .addClasspathResource("calledProcess.bpmn")
+      .deploy();
+
+    // when the called process is started
+    VariableMap startVariables = Variables.createVariables().putValue(CALLING_IN_VARIABLE, INPUT);
+    ProcessInstance callingInstance = callingEngine.getRuntimeService().startProcessInstanceByKey("callingProcess", startVariables);
+
+    waitUntilAllJobsExecuted();
+
+    Task calledTask = calledEngine.getTaskService().createTaskQuery().singleResult();
+
+    // then it has the input mapped variables available
+    Map<String, Object> variablesInCalledProcess = calledEngine.getRuntimeService().getVariables(calledTask.getProcessInstanceId());
+    assertThat(variablesInCalledProcess).containsExactly(entry(CALLED_IN_VARIABLE, INPUT));
+
+    // when the called process finishes
+    VariableMap completeVariables = Variables.createVariables().putValue(CALLED_OUT_VARIABLE, RESULT);
+    calledEngine.getTaskService().complete(calledTask.getId(), completeVariables);
+
+    waitUntilAllJobsExecuted();
+
+    // then the output mapping was applied in the context of the calling process engine
+    Map<String, Object> variablesInCallingProcess = callingEngine.getRuntimeService().getVariables(callingInstance.getId());
+
+    assertThat(variablesInCallingProcess).containsOnly(
+        entry(CALLING_IN_VARIABLE, INPUT),
+        entry(CALLING_OUT_VARIABLE, RESULT),
+        entry(ENGINE_USED_FOR_IN_MAPPING, "callingEngine"),
+        entry(ENGINE_USED_FOR_OUT_MAPPING, "callingEngine"));
+  }
+
+  private void waitUntilAllJobsExecuted() {
+    Awaitility.await()
+      .until(() -> callingEngine.getManagementService().createJobQuery().count() == 0);
   }
 
 }
